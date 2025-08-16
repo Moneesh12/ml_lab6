c696325ca83020248f55785aad41538ede3ea87f
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-08-16 22:41:03 +0530

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 
+#dataset
 rf = pd.read_csv('DCT_mal.csv')
 
+#split the dataset
 x = rf.drop(columns='LABEL').values
 y = rf['LABEL'].values
 
+#entropy formula
 counts = rf['LABEL'].value_counts().values
 probability = counts / counts.sum()
 entropy = -np.sum(probability * np.log2(probability))
