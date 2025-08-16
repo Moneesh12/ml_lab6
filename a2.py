f5bf9a9cd07101410f121da17422682aad522f9b
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-08-16 22:41:27 +0530

    Update a2.py

--- a2.py
+++ a2.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
 import math as m
+
+#dataset
 rf = pd.read_csv('DCT_mal.csv')
 
 x = rf.drop(columns='LABEL').values
 y = rf['LABEL'].values
 
+#finding entroy
 counts = rf['LABEL'].value_counts().values
 probability = counts / counts.sum()
 entropy = np.sum(probability * probability)
 
+#using entropy to find gini 
 gini = m.pow(entropy,2)
 
 gini1 = 1 - entropy
